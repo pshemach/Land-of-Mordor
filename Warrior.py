215b54f341b54f9d5cb22071d85e552365b0c41b
Pasindu <pasindusandakan@gmail.com>
2023-10-01 21:10:20 -0700

    add get_direction method

--- Warrior.py
+++ Warrior.py
@@ -5,6 +5,7 @@ class Warrior:
     def __init__(self, grid_length, grid):
         self.grid_length = grid_length
         self.grid = grid
+        self.move_dic = {'UP': [0, 1], 'DOWN': [0, -1], 'RIGHT': [1, 0], 'LEFT': [-1, 0]}
         self.command = None
         self.current_location = self.initiate_location()
 
@@ -20,21 +21,37 @@ class Warrior:
         self.current_location = self.initiate_location()
         return self.current_location
 
-    def take_step(self, command=None):
+    def get_direction(self):
+        direction_lis = ['UP', 'DOWN', 'LEFT', 'RIGHT']
+        choice = random.choice(direction_lis)
+        for i in range(4):
+            a, b = self.current_location + self.move_dic[choice]
+            if isinstance(self.grid[a, b], Warrior):
+                direction_lis.remove(choice)
+                choice = random.choice(direction_lis)
+                continue
+            else:
+                break
+        return choice
 
+    def take_step(self, command=None):
         self.command = command
-        if self.command == 'UP':
-            if self.current_location[1] != (self.grid_length - 1):
-                self.current_location[1] = self.current_location[1] + 1
-        elif self.command == 'DOWN':
-            if self.current_location[1] != 0:
-                self.current_location[1] = self.current_location[1] - 1
-        elif self.command == 'RIGHT':
-            if self.current_location[0] != (self.grid_length - 1):
-                self.current_location[0] = self.current_location[0] + 1
-        elif self.command == 'LEFT':
-            if self.current_location[0] != 0:
-                self.current_location[0] = self.current_location[0] - 1
+        # if self.command == 'UP':
+        #     if self.current_location[1] != (self.grid_length - 1):
+        #         self.current_location = self.current_location + self.move_dic[command]
+        # elif self.command == 'DOWN':
+        #     if self.current_location[1] != 0:
+        #         self.current_location = self.current_location + self.move_dic[command]
+        # elif self.command == 'RIGHT':
+        #     if self.current_location[0] != (self.grid_length - 1):
+        #         self.current_location = self.current_location + self.move_dic[command]
+        # elif self.command == 'LEFT':
+        #     if self.current_location[0] != 0:
+        #         self.current_location = self.current_location + self.move_dic[command]
+        # return self.current_location
+        a, b = self.current_location + self.move_dic[self.command]
+        if 0 <= a and b <= 9:
+            self.current_location = self.current_location + self.move_dic[self.command]
         return self.current_location
 
     def move_to_mount_doom(self):
