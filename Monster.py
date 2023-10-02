828dad2ae8572b5551e991c7531b7bb77416b176
Pasindu <pasindusandakan@gmail.com>
2023-10-02 01:57:29 -0700

    add move_to_mount_doom method

--- Monster.py
+++ Monster.py
@@ -2,16 +2,18 @@ import random
 
 
 class Monster:
-    def __init__(self, grid_length=10):
+    def __init__(self, grid_length, grid):
         self.grid_length = grid_length
+        self.grid = grid
         self.monster_location = self.locate_monster()
-        while self.monster_location == [(self.grid_length/2)-1, (self.grid_length/2)-1]:
-            self.monster_location = self.locate_monster()
 
     def locate_monster(self):
-        w_i = random.choice([i for i in range(1,self.grid_length-1)])
-        h_i = random.choice([i for i in range(1,self.grid_length-1)])
-        return [w_i, h_i]
+        while True:
+            i_w = random.randint(0, self.grid_length - 1)
+            i_h = random.randint(0, self.grid_length - 1)
+            if self.grid[i_w][i_h] == 0 and (i_w, i_h) != ((self.grid_length/2)-1, (self.grid_length/2)-1):
+                break
+        return [i_w, i_h]
 
     def reset_location(self):
         self.monster_location = self.locate_monster()
--- Warrior.py
+++ Warrior.py
@@ -7,25 +7,28 @@ class Warrior:
         self.grid = grid
         self.move_dic = {'UP': [0, 1], 'DOWN': [0, -1], 'RIGHT': [1, 0], 'LEFT': [-1, 0]}
         self.command = None
-        self.current_location = self.initiate_location()
+        self.warrior_location = self.initiate_location()
 
     def initiate_location(self):
-        i_w = random.randint(0, self.grid_length - 1)
-        if i_w != 0 or i_w != (self.grid_length - 1):
-            i_h = random.choice([0, (self.grid_length - 1)])
-        else:
-            i_h = random.randint(0, self.grid_length - 1)
+        while True:
+            i_w = random.randint(0, self.grid_length - 1)
+            if i_w != 0 or i_w != (self.grid_length - 1):
+                i_h = random.choice([0, (self.grid_length - 1)])
+            else:
+                i_h = random.randint(0, self.grid_length - 1)
+            if self.grid[i_w][i_h] == 0:
+                break
         return [i_w, i_h]
 
     def reset_initial_location(self):
-        self.current_location = self.initiate_location()
-        return self.current_location
+        self.warrior_location = self.initiate_location()
+        return self.warrior_location
 
     def get_direction(self):
         direction_lis = ['UP', 'DOWN', 'LEFT', 'RIGHT']
         choice = random.choice(direction_lis)
         for i in range(4):
-            a, b = self.current_location + self.move_dic[choice]
+            a, b = self.warrior_location + self.move_dic[choice]
             if isinstance(self.grid[a, b], Warrior):
                 direction_lis.remove(choice)
                 choice = random.choice(direction_lis)
@@ -36,15 +39,18 @@ class Warrior:
 
     def take_step(self, command=None):
         self.command = command
-        a, b = self.current_location
+        a, b = self.warrior_location
         if 0 <= a and b <= self.grid_length - 1:
-            self.current_location = self.current_location + self.move_dic[self.command]
-        return self.current_location
+            self.warrior_location = self.warrior_location + self.move_dic[self.command]
+            a, b = self.warrior_location
+            self.grid[a][b] = self
+        return self.warrior_location
 
     def move_to_mount_doom(self):
         while True:
             command = self.get_direction()
             a, b = self.take_step(command=command)
-            if (a,b) == (4,4):
+            print(self.grid)
+            if (a, b) == (4, 4):
                 print('WIN...')
                 break
